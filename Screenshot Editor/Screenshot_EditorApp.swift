a0b45fb7ee68049f1e96a2ca6bed45cff5d6e6bf
Tetsuya81 <tetsuya8109@gmail.com>
2025-03-06 13:02:20 +0900

    Initial Commit

---
+++ Screenshot Editor/Screenshot_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_EditorApp.swift
+//  Screenshot Editor
+//
+//  Created by Tokunaga Tetsuya on 2025/03/06.
+//
+
+import SwiftUI
+
+@main
+struct Screenshot_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Screenshot EditorTests/Screenshot_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_EditorTests.swift
+//  Screenshot EditorTests
+//
+//  Created by Tokunaga Tetsuya on 2025/03/06.
+//
+
+import Testing
+@testable import Screenshot_Editor
+
+struct Screenshot_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
